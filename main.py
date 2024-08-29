ac8d11db8e78101678594eb06c0b500d341d9f58
cbrookscode <colbybrooks@myyahoo.com>
2024-08-29 15:39:50 -0400

    added frame rate limiter (60)

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     #Game Loop
     while 1 > 0:
@@ -14,7 +16,8 @@ def main():
             
         pygame.Surface.fill(screen, (0, 0, 0))
         pygame.display.flip()
-        
+        dt = clock.tick(60)
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
