827c3803c0f7caaffb259b378ee5bc1dcc05798e
cbrookscode <colbybrooks@myyahoo.com>
2024-08-29 15:25:35 -0400

    game loop created and close out button working

--- main.py
+++ main.py
@@ -5,8 +5,16 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    #Game Loop
     while 1 > 0:
-        fill(black, screen)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        pygame.Surface.fill(screen, (0, 0, 0))
+        pygame.display.flip()
+        
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
