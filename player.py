eb59ee0a9f7d5794eafd6ac8605e7749b2badf57
cbrookscode <colbybrooks@myyahoo.com>
2024-09-04 11:19:55 -0400

    should have committed changes earlier. Completed up to Asteroid field added

---
+++ player.py
@@ -0,0 +1,17 @@
+from circleshape import *
+from constants import *
+
+class Player(CircleShape)
+    def __init__(self, x, y):
+        CircleShape(x, y, PLAYER_RADIUS)
+        position = pygame.math.Vector2(x, y)
+        rotation = 0
+    
+    # in the player class
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
